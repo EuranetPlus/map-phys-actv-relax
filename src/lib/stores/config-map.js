8255bc599908e31f0c665ad98610b57c7e198318
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2026-06-18 20:54:25 +0200

    Update map configuration

--- src/lib/stores/config-map.js
+++ src/lib/stores/config-map.js
@@ -1,37 +1,239 @@
 import { writable } from 'svelte/store';
-
 export const mapConfig = writable({
-	title: 'Which EU countries offer the best employment prospects?',
-	subtitle: 'Future employment growth in these countries in 2022-2035 is estimated at 5.6%.',
-	datasetType: 'values',
-	datasetUnit: 'percent',
-	percentRounded: false,
-	colourSchemeType: 'sequential',
-	colourScheme: 'red',
-	colourSchemeClasses: 5,
-	colourSchemeReverse: false,
-	overrideScaleValues: false,
-	colorBarFirstValue: undefined,
-	colorBarLastValue: undefined,
-	headlineAvailable: true,
-	subheadlineAvailable: true,
-	tooltipAvailable: true,
-	scaleBarAvailable: true,
-	legendAvailable: true,
-	textSourceAvailable: true,
-	textSourceDescription: 'Source',
-	textSource: '',
-	textNoteAvailable: true,
-	textNoteDescription: 'Note',
-	textNote: '',
-	textDataAccessAvailable: true,
-	linkDataAccessDescription: 'Access the data',
-	linkDataAccess: '',
-	legend1: 'No data available',
-	legend1Color: '#E0E0E0',
-	tooltipExtraInfoLabel: '',
-	parsedData: null,
-	data: ``,
-	customUnitLabelAvailable: false,
-	customUnitLabel: undefined
+  "title": "How many Europeans are into physical activities as a way to unwind?",
+  "subtitle": "Around four in ten Europeans do sports or physical activities to relax. This is especially true in Cyprus (67%), Slovenia (58%), the Netherlands (56%) and France (50%).",
+  "colourSchemeClasses": 7,
+  "data": "country,id,value,extraInfo,text_content\nBelgium,BE,0.49,false,\nBulgaria,BG,0.33,false,\nCzechia,CZ,0.24,false,\nDenmark,DK,0.23,false,\nGermany,DE,0.41,false,\nEstonia,EE,0.36,false,\nIreland,IE,0.28,false,\nGreece,EL,0.49,false,\nSpain,ES,0.31,false,\nFrance,FR,0.50,false,\nCroatia,HR,0.33,false,\nItaly,IT,0.39,false,\nCyprus,CY,0.67,false,\nLatvia,LV,0.31,false,\nLithuania,LT,0.30,false,\nLuxembourg,LU,0.43,false,\nHungary,HU,0.34,false,\nMalta,MT,0.39,false,\nNetherlands,NL,0.56,false,\nAustria,AT,0.40,false,\nPoland,PL,0.22,false,\nPortugal,PT,0.46,false,\nRomania,RO,0.26,false,\nSlovenia,SI,0.58,false,\nSlovakia,SK,0.20,false,\nFinland,FI,0.41,false,\nSweden,SE,0.28,false,",
+  "parsedData": [
+    {
+      "country": "Belgium",
+      "id": "BE",
+      "value": 0.49,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Bulgaria",
+      "id": "BG",
+      "value": 0.33,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Czechia",
+      "id": "CZ",
+      "value": 0.24,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Denmark",
+      "id": "DK",
+      "value": 0.23,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Germany",
+      "id": "DE",
+      "value": 0.41,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Estonia",
+      "id": "EE",
+      "value": 0.36,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Ireland",
+      "id": "IE",
+      "value": 0.28,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Greece",
+      "id": "EL",
+      "value": 0.49,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Spain",
+      "id": "ES",
+      "value": 0.31,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "France",
+      "id": "FR",
+      "value": 0.5,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Croatia",
+      "id": "HR",
+      "value": 0.33,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Italy",
+      "id": "IT",
+      "value": 0.39,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Cyprus",
+      "id": "CY",
+      "value": 0.67,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Latvia",
+      "id": "LV",
+      "value": 0.31,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Lithuania",
+      "id": "LT",
+      "value": 0.3,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Luxembourg",
+      "id": "LU",
+      "value": 0.43,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Hungary",
+      "id": "HU",
+      "value": 0.34,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Malta",
+      "id": "MT",
+      "value": 0.39,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Netherlands",
+      "id": "NL",
+      "value": 0.56,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Austria",
+      "id": "AT",
+      "value": 0.4,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Poland",
+      "id": "PL",
+      "value": 0.22,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Portugal",
+      "id": "PT",
+      "value": 0.46,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Romania",
+      "id": "RO",
+      "value": 0.26,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Slovenia",
+      "id": "SI",
+      "value": 0.58,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Slovakia",
+      "id": "SK",
+      "value": 0.2,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Finland",
+      "id": "FI",
+      "value": 0.41,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Sweden",
+      "id": "SE",
+      "value": 0.28,
+      "extraInfo": false,
+      "text_content": ""
+    }
+  ],
+  "datasetType": "values",
+  "datasetUnit": "percent",
+  "percentRounded": false,
+  "colourSchemeType": "sequential",
+  "colourScheme": "blue",
+  "colourSchemeReverse": false,
+  "headlineAvailable": true,
+  "subheadlineAvailable": true,
+  "tooltipAvailable": true,
+  "scaleBarAvailable": true,
+  "overrideScaleValues": true,
+  "legendAvailable": true,
+  "textSourceAvailable": true,
+  "textSourceDescription": "Source",
+  "textSource": "Eurobarometer 525 – Sport and Physical Activity",
+  "textNoteAvailable": true,
+  "textNoteDescription": "Note",
+  "textNote": "",
+  "textDataAccessAvailable": true,
+  "linkDataAccessDescription": "Access the data",
+  "linkDataAccess": "https://europa.eu/eurobarometer/api/deliverable/download/file?deliverableId=83654",
+  "legend1": "No data available",
+  "legend1Color": "#E0E0E0",
+  "colorBarFirstValue": "0.2",
+  "colorBarLastValue": "0.7",
+  "customUnitLabelAvailable": false,
+  "customUnitLabel": "",
+  "tooltipExtraInfoLabel": "Click here",
+  "translate": {
+    "title": "How many Europeans are into physical activities as a way to unwind?",
+    "subtitle": "Around four in ten Europeans do sports or physical activities to relax. This is especially true in Cyprus (67%), Slovenia (58%), the Netherlands (56%) and France (50%).",
+    "textNoteDescription": "Note",
+    "textNote": "",
+    "textSourceDescription": "Source",
+    "textSource": "Eurobarometer 525 – Sport and Physical Activity",
+    "linkDataAccessDescription": "Access the data",
+    "legend1": "No data available",
+    "tooltipExtraInfoLabel": "Click here"
+  },
+  "clusters": [],
+  "colorScale": null
 });
